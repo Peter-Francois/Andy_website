56ee353604d88efaa3f911c8a15f8c7a5fbe7645
Peter Francois <peter.francois237@hotmail.com>
2025-01-29 07:58:56 +0100

    update: Menu button now scrolls to the corresponding view

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // Fonction pour afficher le haut de la page
 function displayHome(event) {
     event.preventDefault();
-    const HomeDiv = document.querySelector('div.cover');
+    const HomeDiv = document.querySelector('header');
     HomeDiv.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Fonction pour afficher la page "Savoir faire"
+function displayKnowHow(event) {
+    event.preventDefault();
+    const KnowHowDiv = document.querySelector('div.partie_grise_2');
+    KnowHowDiv.scrollIntoView({ behavior: 'smooth' });
+}
+
+// Fonction pour afficher la page "A propos"
+function displayAbout(event) {
+    event.preventDefault();
+    const AboutDiv = document.querySelector('div.partie_blanche_2');
+    AboutDiv.scrollIntoView({ behavior: 'smooth' });
 }
